a2bb1311bd1755263ae1b26f781b38613b906790
AustineBill <auztine.malic@gmail.com>
2024-01-16 23:05:01 +0800

    updated

--- src/components/BillingOperations.js
+++ src/components/BillingOperations.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Main = styled.div`
@@ -68,7 +68,7 @@ const BillingOperations = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState(null);
 
-  const history = useHistory();
+  const history = useNavigate();
 
   const toggling = () => setIsOpen(!isOpen);
 
