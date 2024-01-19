7b8adf215d2f75b3be43add912bc4d584ea9eeb4
Maviii <144748980+mtuazon@users.noreply.github.com>
2024-01-20 00:14:31 +0800

    Update App.js
    updating nav bar

--- src/App.js
+++ src/App.js
@@ -6,73 +6,29 @@ import Dashboard from './components/Dashboard';
 import BillingOperations from './components/BillingOperations';
 import Receipt from './components/Receipt';
 
-import React, { useState } from 'react';
+
+
 import './App.css';
-import centeredImage from './assets/front-text.png';
+
+import './App.css';
+import Sbsl from './assets/Sbsl.png';
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    // Add registration or login logic here
-  };
-
   return (
-    <div>
-      {/* First Page Section */}
-      <header className="first-page">
-        {/* Image in the center */}
-        <div className="image-container">
-          <img
-            src={centeredImage}
-            alt="Centered Image"
-            className="centered-image"
-          />
-        </div>
-        <div className="login-button">
-          <button>LOGIN</button>
+    <div className="App">
+      <div className="App-navbar">
+        <h1 className="App-title">E-BILLING APP</h1>
+        <button className="App-login-button">LOGIN</button>
+      </div>
+      <header className="App-header">
+        <img src={Sbsl} alt="Sbsl.png" />
+        <div className="App-buttons">
+          <button className="App-register-button">REGISTER HERE</button>
         </div>
       </header>
-
-      {/* Registration Form Section */}
-      <div className="registration-page">
-        <form className="registration-form" onSubmit={handleFormSubmit}>
-          <label htmlFor="username">
-            Username:
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={handleUsernameChange}
-            />
-          </label>
-          <label htmlFor="password">
-            Password:
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-          </label>
-          <button type="submit">REGISTER</button>
-        </form>
-      </div>
     </div>
   );
 }
-
-import './App.css';
 
 const App = () => {
   const buttonStyle = {
