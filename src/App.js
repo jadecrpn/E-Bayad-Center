88d85fa5665c3fdaec374893befb839d0deb57e6
Gwynn Cujardo <gwynn.cujardo@tup.edu.ph>
2024-01-19 10:41:30 +0800

    Installed Some Modules
    Material UI

--- src/App.js
+++ src/App.js
@@ -1,59 +1,59 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RegisterForm from './components/RegisterForm';
-import Dashboard from './components/Dashboard';
-import BillingOperations from './components/BillingOperations';
-import Receipt from './components/Receipt';
+// // src/App.js
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import RegisterForm from './components/RegisterForm';
+// import Dashboard from './components/Dashboard';
+// import BillingOperations from './components/BillingOperations';
+// import Receipt from './components/Receipt';
 
+// import './App.css';
 
+// const App = () => {
+//   return (
+//     <Router>
+//       <div className="container">
+//         <header>
+//           <h1>E-Wallet App</h1>
+//         </header>
+//         <nav>
+//           <a href="/register">Register</a>
+//           <a href="/dashboard">Dashboard</a>
+//           <a href="/billing">Billing</a>
+//           <a href="/receipt">Receipt</a>
+//         </nav>
+//         <Switch>
+//           <Route path="/register" component={RegisterForm} />
+//           <Route path="/dashboard" component={Dashboard} />
+//           <Route path="/billing" component={BillingOperations} />
+//           <Route path="/receipt" component={Receipt} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// };
 
-import './App.css';
+// export default App;
 
-const App = () => {
-  const buttonStyle = {
-    backgroundColor: '#be800d',
-    color: '#000000',
-    border: '1px solid rgb(0, 0, 0)',
-    padding: '10px 20px',
-    textDecoration: 'none',
-    borderRadius: '50px',
-    cursor: 'pointer',
-    display: 'block',
-    margin: '0 auto', // Center the button horizontally
-  };
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
+function ColorSchemesExample() {
   return (
-    <Router> 
-      <div className="container">
-        <nav>
-            <div class = "title"> E-BILLING APP </div> 
-              <a href="/register">Register</a>
-              <a href="/dashboard">Dashboard</a>
-              <a href="/billing">Billing</a>
-              <a href="/receipt">Receipt</a>
-              <button className = "nav-buttons"> <a href="/register" > LOG IN </a></button>  
-        </nav>   
-
-        <div>
-          <button><a href="/register" style={buttonStyle}> REGISTER HERE </a></button>   
-        </div>
-
-         
-        <Routes>
-          <Route path="/register" element={<RegisterForm />} /> 
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/billing"  element={<BillingOperations />} />
-          <Route path="/receipt" element={<Receipt />} />
-        </Routes>
-
-      </div>
-
-      <footer>
-          <p>Copyright &#169; 2024 E-BILLING APP. All Rights Reserved.</p>
-      </footer>
-    </Router>
+    <>
+      <Navbar bg="warning" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      
+    </>
   );
-};
+}
 
-export default App;
+export default ColorSchemesExample;
