a3c01fe04d95d8c4040f6e130603ce82e61e6ce0
Maviii <144748980+mtuazon@users.noreply.github.com>
2024-01-19 18:58:50 +0800

    Update App.js
    update comparing to main

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-// App.js
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RegisterForm from './components/RegisterForm';
+import Dashboard from './components/Dashboard';
+import BillingOperations from './components/BillingOperations';
+import Receipt from './components/Receipt';
 
 import React, { useState } from 'react';
 import './App.css';
@@ -65,5 +71,53 @@ function App() {
     </div>
   );
 }
+
+import './App.css';
+
+const App = () => {
+  const buttonStyle = {
+    backgroundColor: '#be800d',
+    color: '#000000',
+    border: '1px solid rgb(0, 0, 0)',
+    padding: '10px 20px',
+    textDecoration: 'none',
+    borderRadius: '50px',
+    cursor: 'pointer',
+    display: 'block',
+    margin: '0 auto', // Center the button horizontally
+  };
+
+  return (
+    <Router> 
+      <div className="container">
+        <nav>
+            <div class = "title"> E-BILLING APP </div> 
+              <a href="/register">Register</a>
+              <a href="/dashboard">Dashboard</a>
+              <a href="/billing">Billing</a>
+              <a href="/receipt">Receipt</a>
+              <button className = "nav-buttons"> <a href="/register" > LOG IN </a></button>  
+        </nav>   
+
+        <div>
+          <button><a href="/register" style={buttonStyle}> REGISTER HERE </a></button>   
+        </div>
+
+         
+        <Routes>
+          <Route path="/register" element={<RegisterForm />} /> 
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/billing"  element={<BillingOperations />} />
+          <Route path="/receipt" element={<Receipt />} />
+        </Routes>
+
+      </div>
+
+      <footer>
+          <p>Copyright &#169; 2024 E-BILLING APP. All Rights Reserved.</p>
+      </footer>
+    </Router>
+  );
+};
 
 export default App;
