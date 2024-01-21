2ccd6447ea7042b9d7c341988bc0d7a2c98d5816
AustineBill <auztine.malic@gmail.com>
2024-01-21 23:56:00 +0800

    change buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes,} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes,  } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import BillingOperations from './components/BillingOperations';
 import Login from './pages/login';
@@ -13,17 +13,19 @@ import './App.css';
 const App = () => {
 
   return (
+    
         <Router>
             <Routes>
+              
+              <Route path="/" element={<Front />} />
               <Route path="/nav" element={<Navigation />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/billing" element={<BillingOperations />} />
               <Route path="/login" element={<Login />} />
-              <Route path="/front" element={<Front />} />
               <Route path="/signup" element={<Signup />} />
             </Routes>
         </Router>
-  );
+  )
 };
 
 export default App;
