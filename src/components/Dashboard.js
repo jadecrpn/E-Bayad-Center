8951bf7ff7fc116463bb1cc728e090135308b9e0
AustineBill <auztine.malic@gmail.com>
2024-01-21 17:14:14 +0800

    Fix the register form to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import face from '../Assets/face.png';
 
 
 const Dashboard = () => {
+  const [userEmail, setUserEmail] = useState(null);
+  
+  useEffect(() => {
+    const storedEmail = localStorage.getItem('userEmail');
+    setUserEmail(storedEmail);
+  }, []);
+
   const buttonStyle = {
     backgroundColor: 'transparent',
     color: '#000000',
@@ -18,12 +25,12 @@ const Dashboard = () => {
       <nav>
         <div className="title">E-BILLING APP</div>
         <div className="nav-buttons">
-          <a href="/register">LOG OUT</a>
+          <a href="/login">LOG OUT</a>
         </div>
       </nav>
       <section className="Dashboard">
         <div className="About-main">
-          <h1>WELCOME, </h1>
+          <h1>WELCOME,{userEmail && <span>{userEmail}</span>} </h1>
           <p>REDUCE COST, SAVE TIME, AND IMPROVE CASH FLOW WITH E-BILLING APP</p>
           <div className="main_button">
             <a href="/billing" style={buttonStyle}>
