fd3de5af88681280351225b00b3dafefb73b1970
Gwynn Cujardo <gwynn.cujardo@tup.edu.ph>
2024-01-22 21:50:06 +0800

    Updated frontpage.js

--- src/pages/frontpage.js
+++ src/pages/frontpage.js
@@ -1,43 +1,72 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
-import Sbsl from '../Assets/Sbsl.png';
+import Sbsl from "../Assets/Sbsl.png";
 
 const Front = () => {
-  const buttonStyle = {
-    backgroundColor: '#F27C22',
-    color: '#000000',
-    padding: '20px 20px',
-    textDecoration: 'none',
-    borderRadius: '50px',
-    cursor: 'pointer',
-  };
-
   return (
-    <div className="container">
-      <nav>
-        <div className="title">E-BILLING APP</div>
-        <button className="nav-buttons">
-          <Link to="/login" style={buttonStyle}>
-            LOG IN
-          </Link>
-        </button>
-      </nav>
-      <div>
-        <header className="App-header">
-          <img src={Sbsl} alt="Sbsl.png" />
-          <div className="main_button">
-            <Link to="/signup" style={buttonStyle}>
-              REGISTER HERE
-            </Link>
-          </div>
-        </header>
-      </div>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar
+          position="sticky"
+          sx={{ bgcolor: "#F27C22", border: "2px solid #000000" }}
+        >
+          <Toolbar>
+            <Typography
+              variant="h5"
+              component="div"
+              sx={{ flexGrow: 1, fontWeight: "bold", color: "black" }}
+            >
+              E-BILLING APP
+            </Typography>
+            <Button
+              component={Link}
+              to="/login"
+              variant="contained"
+              sx={{
+                bgcolor: "#F27C22",
+                color: "Black",
+                border: "1px solid #000000",
+              }}
+            >
+              Login
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
 
-      <footer>
-        <p>Copyright &#169; 2024 E-BILLING APP. All Rights Reserved.</p>
-      </footer>
-    </div>
+      <Box sx={{ textAlign: "center", marginTop: "2vh" }}>
+        <img
+          src={Sbsl}
+          alt="Sbsl.png"
+          style={{ width: "1025px", height: "576px" }}
+        />
+      </Box>
+
+      <Box sx={{ textAlign: "center" }}>
+        <Button
+          component={Link}
+          to="/signup"
+          variant="contained"
+          sx={{
+            bgcolor: "#F27C22",
+            color: "Black",
+            border: "1px solid #000000",
+            marginTop: "-40vh",
+            width: "200px", // Adjust the width as needed
+            height: "50px", // Adjust the height as needed
+          }}
+        >
+          Register
+        </Button>
+      </Box>
+
+    </>
   );
 };
 
