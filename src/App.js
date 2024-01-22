8cea59644d6ca0bdd024339159166d9b7ca12204
Gwynn Cujardo <gwynn.cujardo@tup.edu.ph>
2024-01-22 13:11:38 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Front from './pages/frontpage';
 import Navigation from './components/Navbar'; 
 
 import './App.css';
+import Transaction from './components/Transaction';
 
 
 const App = () => {
@@ -21,6 +22,7 @@ const App = () => {
               <Route path="/nav" element={<Navigation />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/billing" element={<BillingOperations />} />
+              <Route path="/transaction" element={<Transaction />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
             </Routes>
