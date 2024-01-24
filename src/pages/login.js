0171781c96c650b1ce7c1aba42a425ac2d457c99
Gwynn Cujardo <gwynn.cujardo@tup.edu.ph>
2024-01-24 18:22:43 +0800

    Updated styles

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,106 +1,114 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; 
-import Navigation from '../components/Navbar';
-import { useUser } from '../hooks/useUser'
-import { Toaster } from 'sonner';
-import { toast } from 'sonner';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Navigation from "../components/Navbar";
+import { useUser } from "../hooks/useUser";
+import { Toaster } from "sonner";
+import { toast } from "sonner";
 
+const Login = () => {
+  const navigate = useNavigate();
+  const { loginUser } = useUser(); // Assuming your useUser hook has a function loginUser
 
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const Login = () => {
+  const [errorMessage] = useState("");
 
-        const navigate = useNavigate();
-        const { loginUser } = useUser(); // Assuming your useUser hook has a function loginUser
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
 
-        const [formData, setFormData] = useState({
-            email: '',
-            password: '',
-        });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const [errorMessage] = useState('');
+    // Add validation if email and password are not empty
+    if (!formData.email || !formData.password) {
+      toast.error("Email and password are required.");
+      return;
+    }
 
-        const handleInputChange = (e) => {
-            const { id, value } = e.target;
-            setFormData((prevData) => ({
-                ...prevData,
-                [id]: value,
-            }));
-        };
+    try {
+      // Call the loginUser function from useUser hook
+      const user = await loginUser(formData);
 
-        const handleSubmit = async (e) => {
-            e.preventDefault();
-          
-            // Add validation if email and password are not empty
-            if (!formData.email || !formData.password) {
-              toast.error('Email and password are required.');
-              return;
-            }
+      // If login is successful, you can redirect to the dashboard or do other actions
+      if (user) {
+        navigate("/dashboard"); // Redirect to the dashboard
+        toast.success("Login successful!");
+      } else {
+        // If login is not successful, display an error message
+        toast.error("Invalid email or password. Please try again.");
+      }
+    } catch (error) {
+      console.error("Login failed", error);
+      toast.error("An error occurred during login. Please try again.");
+    }
+  };
 
-            
-        
-            try {
-                // Call the loginUser function from useUser hook
-                const user = await loginUser(formData);
-        
-                // If login is successful, you can redirect to the dashboard or do other actions
-                if (user) {
-                    navigate('/dashboard'); // Redirect to the dashboard
-                    toast.success('Login successful!');
-                } else {
-                    // If login is not successful, display an error message
-                    toast.error('Invalid email or password. Please try again.');
-                }
-            } catch (error) {
-                console.error('Login failed', error);
-                toast.error('An error occurred during login. Please try again.');
-            }
-        };
-       
-        return (
-            <main>
-                <Navigation />
-                <Toaster richColors className='div-container-position' />
-                <div className="div-container1">
-                    {errorMessage && (
-                        <div className="text-red-500 mb-4 border-2 border-red-300 p-4 bg-red-200 rounded-md">
-                            {errorMessage}
-                        </div>
-                    )}
+  return (
+    <main>
+      <Navigation />
+      <Toaster richColors className="div-container-position" />
+      <div className="div-container1" style={{ minHeight: "83.8vh" }}>
+        {errorMessage && (
+          <div className="text-red-500 mb-4 border-2 border-red-300 p-4 bg-red-200 rounded-md">
+            {errorMessage}
+          </div>
+        )}
 
-                    <form onSubmit={handleSubmit}>
-                        <h2 className="head-title">Login</h2>
-                        <div className="outerBox">
-                            <label htmlFor="email" className="labelBox">
-                                Email:
-                            </label>
-                            <input
-                                type="text"
-                                id="email"
-                                value={formData.email}
-                                onChange={handleInputChange}
-                                className="inputBox"
-                            />
-                        </div>
+        <form
+          onSubmit={handleSubmit}
+          style={{ height: "44vh", marginTop: "-10vh" }}
+        >
+          <h2
+            style={{
+              textAlign: "center",
+              marginLeft: "-15vh",
+              marginTop: "0.5vh",
+              marginBottom: "1vh",
+            }}
+            className="head-title"
+          >
+            LOG IN
+          </h2>
+          <div className="outerBox">
+            <label htmlFor="email" className="labelBox">
+              Email:
+            </label>
+            <input
+              type="text"
+              id="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              className="inputBox"
+            />
+          </div>
 
-                        <div className="outerBox">
-                            <label htmlFor="password" className="labelBox">
-                                Password:
-                            </label>
-                            <input
-                                type="password"
-                                id="password"
-                                value={formData.password}
-                                onChange={handleInputChange}
-                                className="inputBox"
-                            />
-                        </div>
-                        <button type="submit" className="signup_button">
-                            Sign In
-                        </button>
-                    </form>
-                </div>
-            </main>
-        );
-    };
+          <div className="outerBox">
+            <label htmlFor="password" className="labelBox">
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              className="inputBox"
+            />
+          </div>
+          <button type="submit" className="signup_button">
+            Sign In
+          </button>
+        </form>
+      </div>
+    </main>
+  );
+};
 
-    export default Login;
+export default Login;
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,126 +1,175 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Assuming you are using react-router
-import { useUser } from '../hooks/useUser';
-import { Toaster } from 'sonner';
-import Navigation from '../components/Navbar';
-import { toast } from 'sonner'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"; // Assuming you are using react-router
+import { useUser } from "../hooks/useUser";
+import { Toaster } from "sonner";
+import Navigation from "../components/Navbar";
+import { toast } from "sonner";
 
 const Signup = () => {
-    const navigate = useNavigate();
-    const { errors, errorMessage} = useUser();
-  
-    const [formData, setFormData] = useState({
-      email: '',
-      password: '',
-      confirmPassword: '',
-    });
-  
-    const handleInputChange = (e) => {
-      const { id, value } = e.target;
-      setFormData((prevData) => ({ ...prevData, [id]: value }));
-    };
-  
-    const handleSignUpAndSave = async (e) => {
-        e.preventDefault();
+  const navigate = useNavigate();
+  const { errors, errorMessage } = useUser();
 
-        // Perform additional validation if needed
-        if (formData.password !== formData.confirmPassword) {
-            // Handle password mismatch
-            toast.error('Passwords do not match');
-            return;
-        }
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-        // Assuming you have a backend server running at http://localhost:3005
-        const apiUrl = 'http://localhost:3005/accounts';
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [id]: value }));
+  };
 
-        try {
-            const response = await fetch(apiUrl, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData),
-            });
+  const handleSignUpAndSave = async (e) => {
+    e.preventDefault();
 
-            if (!response.ok) {
-                // Handle server-side error
-                toast.error('Failed to register user');
-                return;
-            }
+    // Perform additional validation if needed
+    if (formData.password !== formData.confirmPassword) {
+      // Handle password mismatch
+      toast.error("Passwords do not match");
+      return;
+    }
 
-            // Registration successful
-            toast.success('User registered successfully');
-            
-            localStorage.setItem('userEmail', formData.email);
-            navigate('/dashboard');
-            
-        } catch (error) {
-            console.error('Error during registration', error);
-            toast.error('An error occurred');
-        }
-    };
+    // Assuming you have a backend server running at http://localhost:3005
+    const apiUrl = "http://localhost:3005/accounts";
 
+    try {
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
 
-    return (
-        <main>
-            <Navigation />
-            <Toaster richColors position="bottom-right" />
-            <div className="div-container1">
-                {errorMessage && (
-                    <div className="text-red-500 mb-4 border-2 border-red-300 p-4 bg-red-200 rounded-md">
-                        {errorMessage}
-                    </div>
-                )}
-                <form onSubmit={handleSignUpAndSave}>
-                    <h2 className="head-title">Sign Up</h2>
-                        <div className="outerBox">
-                            <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
-                                Email:
-                            </label>
-                            <input
-                                type="text"
-                                id="email"
-                                value={formData.email}
-                                onChange={handleInputChange}
-                                className={`w-full px-3 py-2 border text-black ${errors.email ? 'border-red-500' : 'border-gray-300'} rounded-md`}
-                            />
-                            {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>}
-                        </div>
-                        <div className="outerBox">
-                            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
-                                Password:
-                            </label>
-                            <input
-                                type="password"
-                                id="password"
-                                value={formData.password}
-                                onChange={handleInputChange}
-                                className={`w-full px-3 py-2 border text-black ${errors.password ? 'border-red-500' : 'border-gray-300'} rounded-md`}
-                            />
-                            {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>}
-                        </div>
-                        <div className="outerBox">
-                            <label htmlFor="confirmPassword" className="block text-gray-700 text-sm font-bold mb-2">
-                                Confirm Password:
-                            </label>
-                            <input
-                                type="password"
-                                id="confirmPassword"
-                                value={formData.confirmPassword}
-                                onChange={handleInputChange}
-                                className={`w-full px-3 py-2 border text-black ${errors.confirmPassword ? 'border-red-500' : 'border-gray-300'} rounded-md`}
-                            />
-                            {errors.confirmPassword && <p className="text-red-500 text-sm mt-1">{errors.confirmPassword.message}</p>}
-                        </div>
-                        <button
-                            type="submit" 
-                            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
-                            >Sign Up
-                        </button>
-                    </form>
-                </div>        
-        </main>
-    );
+      if (!response.ok) {
+        // Handle server-side error
+        toast.error("Failed to register user");
+        return;
+      }
+
+      // Registration successful
+      toast.success("User registered successfully");
+
+      localStorage.setItem("userEmail", formData.email);
+      navigate("/dashboard");
+    } catch (error) {
+      console.error("Error during registration", error);
+      toast.error("An error occurred");
+    }
+  };
+
+  return (
+    <main>
+      <Navigation />
+      <Toaster richColors position="bottom-right" />
+      <div className="div-container1" style={{ minHeight: "83.8vh" }}>
+        {errorMessage && (
+          <div className="text-red-500 mb-4 border-2 border-red-300 p-4 bg-red-200 rounded-md">
+            {errorMessage}
+          </div>
+        )}
+        <form
+          onSubmit={handleSignUpAndSave}
+          style={{ height: "54vh", marginTop: "-10vh" }}
+        >
+          <h2
+            style={{
+              textAlign: "center",
+              marginLeft: "-15vh",
+              marginTop: "0.5vh",
+              marginBottom:'1vh'
+            }}
+            className="head-title"
+          >
+            REGISTER
+          </h2>
+          <div className="outerBox">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Email:
+            </label>
+            <input
+              type="text"
+              id="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              className={`w-full px-3 py-2 border text-black ${
+                errors.email ? "border-red-500" : "border-gray-300"
+              } rounded-md`}
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
+          <div className="outerBox">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              className={`w-full px-3 py-2 border text-black ${
+                errors.password ? "border-red-500" : "border-gray-300"
+              } rounded-md`}
+            />
+            {errors.password && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
+          <div className="outerBox">
+            <label
+              htmlFor="confirmPassword"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Confirm Password:
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleInputChange}
+              className={`w-full px-3 py-2 border text-black ${
+                errors.confirmPassword ? "border-red-500" : "border-gray-300"
+              } rounded-md`}
+            />
+            {errors.confirmPassword && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.confirmPassword.message}
+              </p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
+            style={{
+              marginTop: "2vh",
+              width: "20vh",
+              borderRadius: "0px",
+              backgroundColor: "#F27C22",
+              marginLeft: "auto",
+              marginRight: "auto",
+              display: "block",
+              height: "5vh"
+            }}
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </main>
+  );
 };
 
 export default Signup;
